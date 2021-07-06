41f681939a034be7835706a2b45f4cb3effbfcb6
hafizjavaid <hafizjaidi786@gmail.com>
2021-07-07 00:03:37 +0500

    First Commit

---
+++ src/store/index.js
@@ -0,0 +1,160 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import firebase from "firebase/app";
+import "firebase/auth";
+import db from "../firebase/firebaseInit";
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+   
+    blogPosts: [],
+    postLoaded: null,
+    blogHTML: "Write your blog title here...",
+    blogTitle: "",
+    blogPhotoName: "",
+    blogPhotoFileURL: null,
+    blogPhotoPreview: null,
+    editPost: null,
+    user: null,
+    profileAdmin: null,
+    profileEmail: null,
+    profileFirstName: null,
+    profileLastName: null,
+    profileUsername: null,
+    profileId: null,
+    profileInitials: null,
+  },
+  mutations: {
+    // Users Mutations===============
+    setProfileInfo(state, doc) {
+      state.profileId = doc.id;
+      state.profileEmail = doc.data().email;
+      state.profileFirstName = doc.data().firstName;
+      state.profileLastName = doc.data().lastName;
+      state.profileUsername = doc.data().username;
+      console.log(state.profileId);
+    },
+    setProfileInitials(state) {
+      state.profileInitials =
+        state.profileFirstName.trim().charAt(0) +
+        state.profileLastName.trim().charAt(0);
+    },
+    updateUser(state, payload) {
+      state.user = payload;
+    },
+    // Changing the user Information at local Stage
+    changeFirstName(state, payload) {
+      state.profileFirstName = payload;
+    },
+    changeLastName(state, payload) {
+      state.profileLastName = payload;
+    },
+    changeUsername(state, payload) {
+      state.profileUsername = payload;
+    },
+    setProfileAdmin(state, payload) {
+      state.profileAdmin = payload;
+      // console.log();
+    },
+
+    // Users Mutations===============
+
+    newBlogPost(state, payload) {
+      state.blogHTML = payload;
+    },
+    updateBlogTitle(state, payload) {
+      state.blogTitle = payload;
+    },
+    fileNameChange(state, payload) {
+      state.blogPhotoName = payload;
+    },
+    createFileURL(state, payload) {
+      state.blogPhotoFileURL = payload;
+    },
+    openPhotoPreview(state) {
+      state.blogPhotoPreview = !state.blogPhotoPreview;
+    },
+    toggleEditPost(state, payload) {
+      state.editPost = payload;
+    },
+    setBlogState(state, payload) {
+      state.blogTitle = payload.blogTitle;
+      state.blogHTML = payload.blogHTML;
+      state.blogPhotoFileURL = payload.blogCoverPhoto;
+      state.blogPhotoName = payload.blogCoverPhotoName;
+    },
+    filterBlogPost(state, payload) {
+      state.blogPosts = state.blogPosts.filter(
+        (post) => post.blogID !== payload
+      );
+    },
+  },
+  actions: {
+    // User Related Actions ===================
+    async getCurrentUser({ commit }, user) {
+      const database = await db
+        .collection("users")
+        .doc(firebase.auth().currentUser.uid);
+      const dbResults = await database.get();
+      commit("setProfileInfo", dbResults);
+      commit("setProfileInitials");
+      const token = await user.getIdTokenResult();
+      const admin = await token.claims.email;
+      commit("setProfileAdmin", admin);
+    },
+    // Changing the user Information at Firebase Level
+    async updateUserSettings({ commit, state }) {
+      const dataBase = await db.collection("users").doc(state.profileId);
+      await dataBase.update({
+        firstName: state.profileFirstName,
+        lastName: state.profileLastName,
+        username: state.profileUsername,
+      });
+      commit("setProfileInitials");
+    },
+    // User Related Actions ===================
+
+    // Post Related Actions =================
+    async getPost({ state }) {
+      const dataBase = await db.collection("blogPosts").orderBy("date", "desc");
+      const dbResults = await dataBase.get();
+      dbResults.forEach((doc) => {
+        if (!state.blogPosts.some((post) => post.blogID === doc.id)) {
+          const data = {
+            blogID: doc.data().blogID,
+            blogHTML: doc.data().blogHTML,
+            blogCoverPhoto: doc.data().blogCoverPhoto,
+            blogTitle: doc.data().blogTitle,
+            blogDate: doc.data().date,
+            blogCoverPhotoName: doc.data().blogCoverPhotoName,
+          };
+          state.blogPosts.push(data);
+        }
+      });
+      state.postLoaded = true;
+    },
+    async updatePost({ commit, dispatch }, payload) {
+      commit("filterBlogPost", payload);
+      await dispatch("getPost");
+    },
+    async deletePost({ commit }, payload) {
+      const getPost = await db.collection("blogPosts").doc(payload);
+      await getPost.delete();
+      commit("filterBlogPost", payload);
+    },
+
+    // Post Related Actions =================
+  },
+  getters: {
+    blogPostsFeed(state) {
+      return state.blogPosts.slice(0, 2);
+    },
+    blogPostsCards(state) {
+      return state.blogPosts.slice(2, 6);
+    },
+  },
+  modules: {
+    
+  },
+});
